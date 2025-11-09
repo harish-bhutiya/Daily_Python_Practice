9d9d7fcf2aa579aab439f098990dcd86afbae583
harish-bhutiya <harishbhutiya23@gmail.com>
2025-11-09 20:06:42 +0000

    Progress Day 1

---
+++ Python_Basics/sample.py
@@ -0,0 +1,74 @@
+x = 2+2
+print(x)
+y = 2
+print(y)
+
+#Math Operators
+
+#Exponentiation operation : **
+
+x = 2 ** 3
+print(x) #Answer = 8, 2*2*2
+
+#Modulus/remainder : %
+
+x = 25%9 
+print(x) #Answer = 7 , 9*2 = 18, 25-18 = 7
+
+#Integer division = //
+
+x = 25//9
+print(x) #Answer is 2, It is return with round of value
+
+#Division = /
+
+x = 25/9
+print(x) #Answer is 2.7777, It is return with decisimal value
+
+#Multipication = *
+
+x = 2*8
+print(x)
+
+#Subtraction = -
+x = 15-4
+print(x)
+
+#Addition = +
+
+x = 2+13
+print(x)
+
+#Exercise
+
+x = 9%25
+print(x)
+
+x =9//25
+print(x)
+
+x = (25//9)*((25**3)- (150*3)) / (40//3)
+print(x)
+
+#Assignment statements
+
+x = 42
+y = 45
+
+z = x+y #Here x is count as 42 and y as a 45
+z = x*y
+print(z)
+
+x = "Bob"
+y = "Marley"
+
+z = x+y
+print(z)
+
+#First Program
+print("Hello World!")
+print("What is your name?")
+myname = input('>')
+print("It is nice to meet you "+ myname)
+print("The length of your name is:")
+print(len(myname))
