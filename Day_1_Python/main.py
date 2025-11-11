fccffa040bd7281231bab89078c8abef649c236a
harish-bhutiya <harishbhutiya23@gmail.com>
2025-11-11 21:23:05 +0000

    Progress Day 1

---
+++ Day_1_Python/main.py
@@ -0,0 +1,36 @@
+print("Hello World!")
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+# String Manipulation
+
+
+# input() function
+
+#input("What is your name?")
+
+#Print greetings
+
+print("Hello " + input("What is your name?") + "!")
+
+#Variables
+
+name = input("What is your name?")
+print("Hello " + name)
+
+#len() function. it calculates number of character string has
+print(len(name))
+
+username = input("Enter your username")
+length = len(username)
+print(length)
+
+#variable naming, valid type of variable names
+#user_name
+#username
+#userName
+#username1
---
+++ Projects/band_name_generator/main.py
@@ -0,0 +1,5 @@
+print("Hello there")
+city_name = input("Which city are you grew up?\n") #\n is help you to put cursor on new line
+pet_name = input("What is your pet name?\n")
+band_name = "Your band name is "
+print(band_name + city_name+" "+pet_name)
